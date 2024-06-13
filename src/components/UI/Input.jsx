906a06a108c86c38f79c4dd8e9c5f749d487833f
Tom Bielawski <tfbielawski@protonmail.com>
2024-06-12 21:48:15 -0400

    changes to support deployment

--- src/components/UI/Input.jsx
+++
@@ -1,9 +0,0 @@
-export default function Input({label, id, ...props}) {
-    return (
-        <p className="control">
-            <label htmlFor={id}>{label}</label>
-            <input type={props.type} name={id} {...props} required/>
-        </p>
-    )
-}
-
--- src/utils/formatting.js
+++
@@ -1,5 +0,0 @@
-//JS built in number format
-export const currencyFormatter = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "usd"
-})
